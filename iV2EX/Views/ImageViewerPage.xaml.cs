488c74cb62802773a4f9dbfb5600b6d1e4bb305c
HuiZhi <458138254@qq.com>
2026-05-01 23:51:36 +0800

    feat: redesign ImageViewer with floating toolbar, zoom, drag-pan, and clipboard

--- iV2EX/Views/ImageViewerPage.xaml.cs
+++ iV2EX/Views/ImageViewerPage.xaml.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.IO;
-using System.Runtime.InteropServices;
 using Windows.ApplicationModel.DataTransfer;
+using Windows.Foundation;
 using Windows.Storage;
 using Windows.Storage.Streams;
 using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Media.Imaging;
+using iV2EX.GetData;
 
 namespace iV2EX.Views
 {
@@ -15,49 +16,51 @@ namespace iV2EX.Views
     {
         private StorageFile _file;
         private string _imageUrl;
+        private bool _isDragging;
+        private Point _dragStartPoint;
+        private double _dragStartHOffset;
+        private double _dragStartVOffset;
 
         public ImageViewerPage()
         {
             InitializeComponent();
 
-            ShareImage.Click += (s, e) =>
-            {
-                if (DataTransferManager.IsSupported())
-                {
-                    var interop = DataTransferManager.As<IDataTransferManagerInterop>();
-                    var riid = typeof(DataTransferManager).GUID;
-                    var manager = interop.GetForWindow(App.WindowHandle, riid);
-                    manager.DataRequested += (_, args) =>
-                    {
-                        args.Request.Data.Properties.Title = "分享自iV2EX";
-                        args.Request.Data.SetText(_imageUrl);
-                        args.Request.Data.SetBitmap(RandomAccessStreamReference.CreateFromFile(_file));
-                    };
-                    interop.ShowShareUIForWindow(App.WindowHandle);
-                }
-            };
+            ImageScrollViewer.PointerPressed += ImageScrollViewer_PointerPressed;
+            ImageScrollViewer.PointerMoved += ImageScrollViewer_PointerMoved;
+            ImageScrollViewer.PointerReleased += ImageScrollViewer_PointerReleased;
+        }
 
-            SaveImage.Click += async (s, e) =>
-            {
-                try
-                {
-                    var library = await StorageLibrary.GetLibraryAsync(KnownLibraryId.Pictures);
-                    var path = await library.SaveFolder.CreateFolderAsync("iV2EX",
-                        CreationCollisionOption.OpenIfExists);
-                    await _file.CopyAsync(path, _file.Name + Path.GetExtension(_imageUrl),
-                        NameCollisionOption.ReplaceExisting);
-                    ToastTips.ShowTips("已经保存到图片库");
-                }
-                catch
-                {
-                    ToastTips.ShowTips("保存失败");
-                }
-            };
+        private void WrapPanel_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            ImageContainer.Width = e.NewSize.Width;
+            ImageContainer.Height = e.NewSize.Height;
+        }
 
-            MenuItemPanel.Tapped += (s, e) =>
-            {
-                MenuItemPanel.ContextFlyout.ShowAt(MenuItemPanel);
-            };
+        private void ImageScrollViewer_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            _isDragging = true;
+            _dragStartPoint = e.GetCurrentPoint(ImageScrollViewer).Position;
+            _dragStartHOffset = ImageScrollViewer.HorizontalOffset;
+            _dragStartVOffset = ImageScrollViewer.VerticalOffset;
+            ImageScrollViewer.CapturePointer(e.Pointer);
+        }
+
+        private void ImageScrollViewer_PointerMoved(object sender, PointerRoutedEventArgs e)
+        {
+            if (!_isDragging) return;
+            var position = e.GetCurrentPoint(ImageScrollViewer).Position;
+            var deltaX = _dragStartPoint.X - position.X;
+            var deltaY = _dragStartPoint.Y - position.Y;
+            ImageScrollViewer.ChangeView(
+                _dragStartHOffset + deltaX,
+                _dragStartVOffset + deltaY,
+                null);
+        }
+
+        private void ImageScrollViewer_PointerReleased(object sender, PointerRoutedEventArgs e)
+        {
+            _isDragging = false;
+            ImageScrollViewer.ReleasePointerCapture(e.Pointer);
         }
 
         protected override async void OnNavigatedTo(Microsoft.UI.Xaml.Navigation.NavigationEventArgs e)
@@ -69,17 +72,87 @@ namespace iV2EX.Views
             {
                 if (_imageUrl.StartsWith("//"))
                     _imageUrl = "https:" + _imageUrl;
+
                 ImagePanel.Source = new BitmapImage(new Uri(_imageUrl));
+                ImageScrollViewer.ChangeView(null, null, 1.0f);
+
+                try
+                {
+                    using var stream = await ApiClient.GetStream(_imageUrl);
+                    var extension = Path.GetExtension(new Uri(_imageUrl).AbsolutePath);
+                    if (string.IsNullOrEmpty(extension))
+                        extension = ".png";
+                    _file = await ApplicationData.Current.TemporaryFolder.CreateFileAsync(
+                        $"iv2ex_image{extension}", CreationCollisionOption.ReplaceExisting);
+                    using var fileStream = await _file.OpenStreamForWriteAsync();
+                    await stream.CopyToAsync(fileStream);
+                }
+                catch
+                {
+                    // download failed, clipboard/save will show error toast
+                }
             }
         }
-    }
 
-    [ComImport]
-    [Guid("3A3DCD6C-3EAB-43DC-BCDE-45671CE800C8")]
-    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
-    interface IDataTransferManagerInterop
-    {
-        DataTransferManager GetForWindow(IntPtr appWindow, [In] ref Guid riid);
-        void ShowShareUIForWindow(IntPtr appWindow);
+        private void ImageScrollViewer_ViewChanged(object sender, ScrollViewerViewChangedEventArgs e)
+        {
+            var zoom = ImageScrollViewer.ZoomFactor;
+            ZoomPercentageText.Text = $"{zoom * 100:0}%";
+        }
+
+        private void ZoomInButton_Click(object sender, RoutedEventArgs e)
+        {
+            var current = (double)ImageScrollViewer.ZoomFactor;
+            var newZoom = (float)Math.Min(current + 0.1f, ImageScrollViewer.MaxZoomFactor);
+            ImageScrollViewer.ChangeView(null, null, newZoom);
+        }
+
+        private void ZoomOutButton_Click(object sender, RoutedEventArgs e)
+        {
+            var current = (double)ImageScrollViewer.ZoomFactor;
+            var newZoom = (float)Math.Max(current - 0.1f, ImageScrollViewer.MinZoomFactor);
+            ImageScrollViewer.ChangeView(null, null, newZoom);
+        }
+
+        private async void CopyButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (_file == null)
+            {
+                ToastTips.ShowTips("图片未加载完成");
+                return;
+            }
+            try
+            {
+                var dataPackage = new DataPackage();
+                dataPackage.SetBitmap(RandomAccessStreamReference.CreateFromFile(_file));
+                Clipboard.SetContent(dataPackage);
+                ToastTips.ShowTips("已复制到剪贴板");
+            }
+            catch
+            {
+                ToastTips.ShowTips("复制失败");
+            }
+        }
+
+        private async void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (_file == null)
+            {
+                ToastTips.ShowTips("图片未加载完成");
+                return;
+            }
+            try
+            {
+                var library = await StorageLibrary.GetLibraryAsync(KnownLibraryId.Pictures);
+                var path = await library.SaveFolder.CreateFolderAsync("iV2EX",
+                    CreationCollisionOption.OpenIfExists);
+                await _file.CopyAsync(path, _file.Name, NameCollisionOption.ReplaceExisting);
+                ToastTips.ShowTips("已经保存到图片库");
+            }
+            catch
+            {
+                ToastTips.ShowTips("保存失败");
+            }
+        }
     }
 }
