aed3a24e91a51383a5c9683ad6f86576e3ed11de
HuiZhi <458138254@qq.com>
2026-07-18 01:43:37 +0800

    feat:Redesign MoneyDetailView with Fluent card-style transaction list

--- iV2EX/Util/Converter.cs
+++ iV2EX/Util/Converter.cs
@@ -9,8 +9,26 @@ namespace iV2EX.Util
         public static SolidColorBrush SymbolToColor(string s)
         {
             return s.StartsWith("-")
-                ? new SolidColorBrush(Color.FromArgb(255, 255, 60, 112))
-                : new SolidColorBrush(Color.FromArgb(255, 10, 163, 77));
+                ? new SolidColorBrush(Color.FromArgb(255, 229, 57, 53))
+                : new SolidColorBrush(Color.FromArgb(255, 67, 160, 71));
+        }
+
+        public static SolidColorBrush SymbolToLightColor(string s)
+        {
+            return s.StartsWith("-")
+                ? new SolidColorBrush(Color.FromArgb(36, 229, 57, 53))
+                : new SolidColorBrush(Color.FromArgb(36, 67, 160, 71));
+        }
+
+        public static string TypeToGlyph(string s)
+        {
+            if (s.Contains("登录") || s.Contains("签到")) return "\uE787";
+            if (s.Contains("活跃")) return "\uE945";
+            if (s.Contains("谢意")) return "\uEB51";
+            if (s.Contains("收益")) return "\uE8BD";
+            if (s.Contains("回复")) return "\uE90A";
+            if (s.Contains("主题")) return "\uE70F";
+            return "\uE825";
         }
 
         public static string CheckedToString(bool check)
